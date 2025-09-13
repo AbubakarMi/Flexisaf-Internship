2e03705755515ccab515c15282c8e5fed4916fb7
Abubakar Mi <abubakarmi131@gmail.com>
2025-09-13 15:26:59 +0100

    money

--- src/com/moneycalculator/Money.java
+++ src/com/moneycalculator/Money.java
@@ -1,3 +1,24 @@
+package com.moneycalculator;
+
 public class Money {
-    
+    private final double amount;
+    private final Currency currency;
+
+    public Money(double amount, Currency currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%.2f %s", amount, currency.getSymbol());
+    }
 }
